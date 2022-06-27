6b6f73b6187568e216adea41bc13bdd3e2eb83ca
Hafiz Muhammad Rayyan <hafizmuhammadrayyan@gmail.com>
2022-06-27 22:21:42 +0500

    2nd

--- script.js
+++ script.js
@@ -1,145 +1,135 @@
-    let input1 = "";
-    let input2 = "";
+let input1 = "";
+let input2 = "";
 
-        function start() {
+function start() {
 
-            let message = document.querySelector("#message")
-            let mInput1 = document.querySelector("#mInput1")
-            let mInput2 = document.querySelector("#mInput2")
-            let mResult = document.querySelector("#mResult")
+    let message = document.querySelector("#message")
+    let mInput1 = document.querySelector("#mInput1")
+    let mInput2 = document.querySelector("#mInput2")
+    let mResult = document.querySelector("#mResult")
 
-            mInput1.innerHTML = "";
-            mInput2.innerHTML = "";
-            mResult.innerHTML = "";
-
-
-            input1 = +document.querySelector("#input1").value;
-            input2 = +document.querySelector("#input2").value;
-
-            if (input1 !== input2) {
-                alert("Please enter the same number for both inputs");
-                return;
-            }
-            if (isNaN(input1) || isNaN(input2)) {
-                alert("Please enter a number");
-                return;
-            }
-            if (input1 < 2 || input2 < 2) {
-                alert("Please enter a number greater than 1");
-                return;
-            }
+    mInput1.innerHTML = "";
+    mInput2.innerHTML = "";
+    mResult.innerHTML = "";
 
 
-            for (let i = 0; i < input1; i++) {
-                for (let j = 0; j < input2; j++) {
-                    console.log("running");
-                    mInput1.innerHTML +=
-                        `<input type="number" required min="0" max="100" id="m1-${i}-${j}" value="0">`;
-                    mInput2.innerHTML +=
-                        `<input type="number" required min="0" max="100" id="m2-${i}-${j}" value="0">`;
-                    mResult.innerHTML +=
-                        `<input type="number" disabled required min="0" max="100" id="mResult-${i}-${j}" value="0">`;
-                }
-                mInput1.innerHTML += `<br>`;
-                mInput2.innerHTML += `<br>`;
-                mResult.innerHTML += `<br>`;
-            }
-            document.querySelector("#metrixInputForm").classList.remove("hidden");
+    input1 = +document.querySelector("#input1").value;
+    input2 = +document.querySelector("#input2").value;
 
+    if (input1 !== input2) {
+        alert("Please enter the same number for both inputs");
+        return;
+    }
+    if (isNaN(input1) || isNaN(input2)) {
+        alert("Please enter a number");
+        return;
+    }
+    if (input1 < 2 || input2 < 2) {
+        alert("Please enter a number greater than 1");
+        return;
+    }
+
+
+    for (let i = 0; i < input1; i++) {
+        for (let j = 0; j < input2; j++) {
+            console.log("running");
+            mInput1.innerHTML +=
+                `<input type="number" required min="0" max="100" id="m1-${i}-${j}" value="0">`;
+            mInput2.innerHTML +=
+                `<input type="number" required min="0" max="100" id="m2-${i}-${j}" value="0">`;
+            mResult.innerHTML +=
+                `<input type="number" disabled required min="0" max="100" id="mResult-${i}-${j}" value="0">`;
         }
+        mInput1.innerHTML += `<br>`;
+        mInput2.innerHTML += `<br>`;
+        mResult.innerHTML += `<br>`;
+    }
+    document.querySelector("#metrixInputForm").classList.remove("hidden");
+
+}
 
 
-        function sum() {
+function sum() {
 
-            let matrix1 = [];
-            let matrix2 = [];
+    let matrix1 = [];
+    let matrix2 = [];
 
-            for (let i = 0; i < input1; i++) {
-                for (let j = 0; j < input2; j++) {
-                    console.log("running");
-                    if (matrix1[i] === undefined) matrix1[i] = []
-                    if (matrix2[i] === undefined) matrix2[i] = []
+    for (let i = 0; i < input1; i++) {
+        for (let j = 0; j < input2; j++) {
+            console.log("running");
+            if (matrix1[i] === undefined) matrix1[i] = []
+            if (matrix2[i] === undefined) matrix2[i] = []
 
-                    matrix1[i][j] = +document.querySelector(`#m1-${i}-${j}`).value
-                    matrix2[i][j] = +document.querySelector(`#m2-${i}-${j}`).value
-                }
-            }
- 
-            let select = document.getElementById('drop_down');
-            let value = select.options[select.selectedIndex].value;
-            console.log(value);
-        
-            if (value == "+") {
-                let result = sumMatrix(matrix1, matrix2);
-                for (let i = 0; i < result.length; i++) {
-                    for (let j = 0; j < result.length; j++) {
-                        document.querySelector(`#sum_input_${i}_${j}`).value = result[i][j];
-                    }
-                }
-        
-            }
-        
-            else if (value == "-") {
-                let result = subMatrix(matrix1, matrix2);
-                for (let i = 0; i < result.length; i++) {
-                    for (let j = 0; j < result.length; j++) {
-                        document.querySelector(`#sum_input_${i}_${j}`).value = result[i][j];
-                    }
-                }
-        
-            }
-        
-            else if (value == "x") {
-                let result = mulMatrix(matrix1, matrix2);
-                for (let i = 0; i < result.length; i++) {
-                    for (let j = 0; j < result.length; j++) {
-                        document.querySelector(`#sum_input_${i}_${j}`).value = result[i][j];
-                    }
-                }
-        
+            matrix1[i][j] = +document.querySelector(`#m1-${i}-${j}`).value
+            matrix2[i][j] = +document.querySelector(`#m2-${i}-${j}`).value
+        }
+    }
+
+    let select = document.getElementById('drop_down');
+    let value = select.options[select.selectedIndex].value;
+    console.log(value);
+
+    if (value == "+") {
+        let result = sumMatrix(matrix1, matrix2);
+    }
+
+    else if (value == "-") {
+        let result = minusMatrix(matrix1, matrix2);
+    }
+
+    else if (value == "x") {
+        let result = multiplyMatrix(matrix1, matrix2);
+    }
+
+    for (let i = 0; i < result.length; i++) {
+        for (let j = 0; j < result.length; j++) {
+            document.querySelector(`#mResult_${i}_${j}`).value = result[i][j];
+        }
+    }
+}
+
+
+
+
+
+// ___________ Funtions ___________
+
+function sumMatrix(matrix1, matrix2) {
+    let result = [];
+    for (let i = 0; i < matrix1.length; i++) {
+        result[i] = [];
+        for (let j = 0; j < matrix1[i].length; j++) {
+            result[i][j] = matrix1[i][j] + matrix2[i][j];
+        }
+    }
+    return result;
+}
+
+
+function minusMatrix(matrix1, matrix2) {
+    let result = [];
+    for (let i = 0; i < matrix1.length; i++) {
+        result[i] = [];
+        for (let j = 0; j < matrix1[i].length; j++) {
+            result[i][j] = matrix1[i][j] - matrix2[i][j];
+        }
+    }
+    return result;
+}
+
+
+function multiplyMatrix(matrix1, matrix2) {
+    var aNumRows = matrix1.length, aNumCols = matrix1[0].length,
+        bNumRows = matrix2.length, bNumCols = matrix2[0].length,
+        result = new Array(aNumRows);  // initialize array of rows
+    for (var r = 0; r < aNumRows; ++r) {
+        result[r] = new Array(bNumCols); // initialize the current row
+        for (var c = 0; c < bNumCols; ++c) {
+            result[r][c] = 0;             // initialize the current cell
+            for (var i = 0; i < aNumCols; ++i) {
+                result[r][c] += matrix1[r][i] * matrix2[i][c];
             }
         }
-        
-
-
-
-
-        // _________ Funtions _________
-
-        function sumMatrix(matrix1, matrix2) {
-            let result = [];
-            for (let i = 0; i < matrix1.length; i++) {
-                result[i] = [];
-                for (let j = 0; j < matrix1[i].length; j++) {
-                    result[i][j] = matrix1[i][j] + matrix2[i][j];
-                }
-            }
-            return result;
-        }
-        
-        function minusMatrix(matrix1, matrix2) {
-            let result = [];
-            for (let i = 0; i < matrix1.length; i++) {
-                result[i] = [];
-                for (let j = 0; j < matrix1[i].length; j++) {
-                    result[i][j] = matrix1[i][j] - matrix2[i][j];
-                }
-            }
-            return result;
-        }
-        
-        function multiplyMatrix(matrix1, matrix2) {
-            var aNumRows = matrix1.length, aNumCols = matrix1[0].length,
-                bNumRows = matrix2.length, bNumCols = matrix2[0].length,
-                result = new Array(aNumRows);  // initialize array of rows
-            for (var r = 0; r < aNumRows; ++r) {
-                result[r] = new Array(bNumCols); // initialize the current row
-                for (var c = 0; c < bNumCols; ++c) {
-                    result[r][c] = 0;             // initialize the current cell
-                    for (var i = 0; i < aNumCols; ++i) {
-                        result[r][c] += matrix1[r][i] * matrix2[i][c];
-                    }
-                }
-            }
-            return result;
-        }
+    }
+    return result;
+}
